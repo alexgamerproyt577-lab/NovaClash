e0613fbf72ab0535d572f40a92a11d185ff5f4a4
alexgamerproyt577-lab <alexgamerproyt577@gmail.com>
2026-07-04 19:16:09 -0300

    Create resultados.js

---
+++ resultados.js
@@ -0,0 +1,12 @@
+const resultados = {
+  1: {
+    ganador: "Los Squidibi Sigma",
+    resultado: "2-1"
+  },
+  2: {
+    ganador: "The gods of war",
+    resultado: "2-0"
+  }
+};
+
+export default resultados;
