30e083259b329dfa395eed53c199299b536b471a
alexgamerproyt577-lab <alexgamerproyt577@gmail.com>
2026-07-04 11:27:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-const boton = document.getElementById("comenzar");
+const boton = document.querySelector("button");
 
-if (boton) {
-    boton.addEventListener("click", () => {
-        window.location.href = "predicciones.html";
-    });
-}
+boton.addEventListener("mousemove", (e)=>{
+
+const x = e.offsetX;
+const y = e.offsetY;
+
+boton.style.background =
+`radial-gradient(circle at ${x}px ${y}px,#b54cff,#5b2cff)`;
+
+});
+
+boton.addEventListener("mouseleave",()=>{
+
+boton.style.background =
+"linear-gradient(90deg,#5b2cff,#aa3fff)";
+
+});
