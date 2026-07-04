50c75231890931fe63974869432d11b7e6adedb9
alexgamerproyt577-lab <alexgamerproyt577@gmail.com>
2026-07-04 11:38:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,86 @@
-const boton = document.querySelector("button");
+const equipos = [
 
-boton.addEventListener("mousemove", (e)=>{
+["Equipo 1","Equipo 2"],
+["Equipo 3","Equipo 4"],
+["Equipo 5","Equipo 6"],
+["Equipo 7","Equipo 8"],
+["Equipo 9","Equipo 10"],
+["Equipo 11","Equipo 12"],
+["Equipo 13","Equipo 14"],
+["Equipo 15","Equipo 16"],
+["Equipo 17","Equipo 18"],
+["Equipo 19","Equipo 20"],
+["Equipo 21","Equipo 22"],
+["Equipo 23","Equipo 24"],
+["Equipo 25","Equipo 26"],
+["Equipo 27","Equipo 28"],
+["Equipo 29","Equipo 30"],
+["Equipo 31","Equipo 32"]
 
-const x = e.offsetX;
-const y = e.offsetY;
+];
 
-boton.style.background =
-`radial-gradient(circle at ${x}px ${y}px,#b54cff,#5b2cff)`;
+const contenedor=document.getElementById("partidos");
+
+if(contenedor){
+
+equipos.forEach((partido,i)=>{
+
+contenedor.innerHTML+=`
+
+<div class="partido">
+
+<h2>Partido ${i+1}</h2>
+
+<p><b>${partido[0]}</b> VS <b>${partido[1]}</b></p>
+
+<label>
+
+<input type="radio" name="g${i}" value="${partido[0]}">
+
+${partido[0]}
+
+</label>
+
+<label>
+
+<input type="radio" name="g${i}" value="${partido[1]}">
+
+${partido[1]}
+
+</label>
+
+<br>
+
+<label>
+
+<input type="radio" name="r${i}" value="2-0">
+
+2-0
+
+</label>
+
+<label>
+
+<input type="radio" name="r${i}" value="2-1">
+
+2-1
+
+</label>
+
+</div>
+
+<br>
+
+`;
 
 });
 
-boton.addEventListener("mouseleave",()=>{
+}
 
-boton.style.background =
-"linear-gradient(90deg,#5b2cff,#aa3fff)";
+function enviarPredicciones(){
 
-});
+const nombre=document.getElementById("nombre").value;
+
+alert("¡Gracias por participar, "+nombre+"!");
+
+}
