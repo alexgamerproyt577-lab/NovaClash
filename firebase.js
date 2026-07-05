1c8fd57b61b733164f5a5f86dfa9dbb0961d3672
alexgamerproyt577-lab <alexgamerproyt577@gmail.com>
2026-07-05 11:24:24 -0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
+
+import {
+    getFirestore
+} from "https://www.gstatic.com/firebasejs/12.5.0/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCULLdWfSlZL2vNetwknkaEOj5fAsIIr6o",
+    authDomain: "nova-clash-8c78b.firebaseapp.com",
+    projectId: "nova-clash-8c78b",
+    storageBucket: "nova-clash-8c78b.firebasestorage.app",
+    messagingSenderId: "202679377197",
+    appId: "1:202679377197:web:b5ed1526028cac92aa07a4"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
