dae6e21bf6ab22412e8e66ba41430215fc77c445
alexgamerproyt577-lab <alexgamerproyt577@gmail.com>
2026-07-05 11:26:35 -0300

    Create equipos.js

---
+++ equipos.js
@@ -0,0 +1,26 @@
+export const equiposBloque1 = [
+["Los Squidibi Sigma","Gigachad"],
+["CPH Brazinos","The gods of war"],
+["Team Yetis","The Thunders"],
+["Equipo 7","Equipo 8"],
+["Equipo 9","Equipo 10"],
+["Equipo 11","Equipo 12"],
+["Equipo 13","Equipo 14"],
+["Equipo 15","Equipo 16"]
+];
+
+export const equiposBloque2 = [
+["Equipo 17","Equipo 18"],
+["Equipo 19","Equipo 20"],
+["Equipo 21","Equipo 22"],
+["Equipo 23","Equipo 24"],
+["Equipo 25","Equipo 26"],
+["Equipo 27","Equipo 28"],
+["Equipo 29","Equipo 30"],
+["Equipo 31","Equipo 32"]
+];
+
+export const todosLosEquipos = [
+...equiposBloque1,
+...equiposBloque2
+];
