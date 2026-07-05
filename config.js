bc8deb0354f2a6b41fbfd87ed1e33a8f2161b24f
alexgamerproyt577-lab <alexgamerproyt577@gmail.com>
2026-07-05 13:04:28 -0300

    Update config.js

--- config.js
+++ config.js
@@ -3,3 +3,9 @@ export const BLOQUE_ACTIVO = 1;
 export const PUNTOS_GANADOR = 2;
 
 export const PUNTOS_RESULTADO = 1;
+
+export const NOMBRE_TORNEO = "NOVA CLASH";
+
+export const PREMIO = "BRAWL PASS PLUS";
+
+export const ADMIN_PASSWORD = "Nova2026";
