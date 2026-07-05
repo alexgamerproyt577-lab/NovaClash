f89d9f221af743e6c2b4e650ad277bb5336af1c5
alexgamerproyt577-lab <alexgamerproyt577@gmail.com>
2026-07-05 16:27:40 -0300

    Update bracket.js

--- bracket.js
+++ bracket.js
@@ -1,8 +1,16 @@
 import { equiposBloque1 } from "./equipos.js";
 
 const r16 = document.getElementById("r16");
+const r8 = document.getElementById("r8");
+const r4 = document.getElementById("r4");
+const r2 = document.getElementById("r2");
+const r1 = document.getElementById("r1");
 
-equiposBloque1.forEach((partido)=>{
+/* =========================
+   16avos
+========================= */
+
+equiposBloque1.forEach((partido) => {
 
     r16.innerHTML += `
 
@@ -17,3 +25,75 @@ equiposBloque1.forEach((partido)=>{
     `;
 
 });
+
+/* =========================
+   Octavos
+========================= */
+
+for(let i=0;i<4;i++){
+
+    r8.innerHTML += `
+
+    <div class="match">
+
+        <div class="equipo">------</div>
+
+        <div class="equipo">------</div>
+
+    </div>
+
+    `;
+
+}
+
+/* =========================
+   Cuartos
+========================= */
+
+for(let i=0;i<2;i++){
+
+    r4.innerHTML += `
+
+    <div class="match">
+
+        <div class="equipo">------</div>
+
+        <div class="equipo">------</div>
+
+    </div>
+
+    `;
+
+}
+
+/* =========================
+   Semifinal
+========================= */
+
+r2.innerHTML = `
+
+<div class="match">
+
+<div class="equipo">------</div>
+
+<div class="equipo">------</div>
+
+</div>
+
+`;
+
+/* =========================
+   Final
+========================= */
+
+r1.innerHTML = `
+
+<div class="match">
+
+<div class="equipo">🏆</div>
+
+<div class="equipo">🏆</div>
+
+</div>
+
+`;
