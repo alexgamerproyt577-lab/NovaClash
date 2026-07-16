68ff608a304e1014fcdb86166fe504c46ae7cc54
alexgamerproyt577-lab <alexgamerproyt577@gmail.com>
2026-07-16 18:44:36 -0300

    Update equipos.js

--- equipos.js
+++ equipos.js
@@ -1,26 +1,43 @@
-export const equiposBloque1 = [
-["Los Squidibi Sigma","Gigachad"],
-["CPH Brazinos","The gods of war"],
-["Team Yetis","The Thunders"],
-["Heirs Of J.P","Los Campeones"],
-["Shirin Ryu Gaming","Cat Ezports"],
-["Los Fracasados","Dark Domain"],
-["Tatakae","Jema Chicer"],
-["Community-8","MJX"]
-];
+export const grupos = {
+  A: [
+    "Team Yetis",
+    "Black Cat",
+    "CPH Brazinos",
+    "The Thunders"
+  ],
 
-export const equiposBloque2 = [
-["Team Cheddar","Krew Rabels"],
-["SX","Tilines De Guadalajara"],
-["Ultra Chmicha","Equipo 22"],
-["Equipo 23","The Fenix"],
-["Equipo 25","Equipo 26"],
-["Equipo 27","Equipo 28"],
-["Equipo 29","Equipo 30"],
-["Equipo 31","Equipo 32"]
-];
+  B: [
+    "The Gods Of War",
+    "Shirin Ryu Gaming",
+    "Heirs Of J.P",
+    "Team Cheddar"
+  ],
 
-export const todosLosEquipos = [
-...equiposBloque1,
-...equiposBloque2
-];
+  C: [
+    "Cat Ezports",
+    "Gigachad",
+    "Los Campeones",
+    "The Squidibi Sigma"
+  ],
+
+  D: [
+    "Jema Chicer",
+    "MJX",
+    "Los Fracasados",
+    "Tatakae"
+  ],
+
+  E: [
+    "Community 8",
+    "Krew Rabels",
+    "TPE",
+    "Dark Domain"
+  ],
+
+  F: [
+    "Tilines De Guadalajara",
+    "Los Indestructibles",
+    "Ultra Chmicha",
+    "SX"
+  ]
+};
