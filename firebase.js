79c5c416a48ecfae684589a42e98698962d00be8
alexgamerproyt577-lab <alexgamerproyt577@gmail.com>
2026-07-17 18:51:45 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,18 +3,6 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/12.5.0/firebase
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
 
 const firebaseConfig = {
-
-  // tu configuración
-
-};
-
-const app = initializeApp(firebaseConfig);
-
-export const db = getFirestore(app);
-
-export const auth = getAuth(app);
-
-const firebaseConfig = {
     apiKey: "AIzaSyCULLdWfSlZL2vNetwknkaEOj5fAsIIr6o",
     authDomain: "nova-clash-8c78b.firebaseapp.com",
     projectId: "nova-clash-8c78b",
@@ -23,8 +11,11 @@ const firebaseConfig = {
     appId: "1:202679377197:web:b5ed1526028cac92aa07a4"
 };
 
+// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
 
-const db = getFirestore(app);
+// Firestore
+export const db = getFirestore(app);
 
-export { db };
+// Authentication
+export const auth = getAuth(app);
