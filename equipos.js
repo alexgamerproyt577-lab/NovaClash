ad76a4cce6aae3ca5eee75483b9ebe5465e0bcda
alexgamerproyt577-lab <alexgamerproyt577@gmail.com>
2026-07-16 22:42:18 -0300

    Update equipos.js

--- equipos.js
+++ equipos.js
@@ -1,3 +1,7 @@
+/* ==========================================
+   EQUIPOS - NOVA CLASH 2026
+========================================== */
+
 export const grupos = {
 
 A: [
@@ -38,5 +42,112 @@ D: [
 
 };
 
-// Lista automática de grupos
+/* ==========================================
+   NOMBRES DE LOS GRUPOS
+========================================== */
+
 export const nombresGrupos = Object.keys(grupos);
+
+/* ==========================================
+   PARTIDOS DE LA FASE DE GRUPOS
+========================================== */
+
+export const partidosGrupos = {
+
+A: [
+
+["The Thunders","CPH Brazin"],
+["The Thunders","Team Yetis"],
+["The Thunders","SX"],
+["The Thunders","Heirs Of J.P"],
+["The Thunders","The Skidibi Sigma"],
+
+["CPH Brazin","Team Yetis"],
+["CPH Brazin","SX"],
+["CPH Brazin","Heirs Of J.P"],
+["CPH Brazin","The Skidibi Sigma"],
+
+["Team Yetis","SX"],
+["Team Yetis","Heirs Of J.P"],
+["Team Yetis","The Skidibi Sigma"],
+
+["SX","Heirs Of J.P"],
+["SX","The Skidibi Sigma"],
+
+["Heirs Of J.P","The Skidibi Sigma"]
+
+],
+
+B: [
+
+["Gigachad","The Gods of War"],
+["Gigachad","Los Campeones"],
+["Gigachad","Shirin Ryu Gaming"],
+["Gigachad","Cat Ezports"],
+["Gigachad","Los Fracasados"],
+
+["The Gods of War","Los Campeones"],
+["The Gods of War","Shirin Ryu Gaming"],
+["The Gods of War","Cat Ezports"],
+["The Gods of War","Los Fracasados"],
+
+["Los Campeones","Shirin Ryu Gaming"],
+["Los Campeones","Cat Ezports"],
+["Los Campeones","Los Fracasados"],
+
+["Shirin Ryu Gaming","Cat Ezports"],
+["Shirin Ryu Gaming","Los Fracasados"],
+
+["Cat Ezports","Los Fracasados"]
+
+],
+
+C: [
+
+["Dark Domain","Tatakae"],
+["Dark Domain","MJX"],
+["Dark Domain","Jema Chicer"],
+["Dark Domain","Community 8"],
+["Dark Domain","Black Cat"],
+
+["Tatakae","MJX"],
+["Tatakae","Jema Chicer"],
+["Tatakae","Community 8"],
+["Tatakae","Black Cat"],
+
+["MJX","Jema Chicer"],
+["MJX","Community 8"],
+["MJX","Black Cat"],
+
+["Jema Chicer","Community 8"],
+["Jema Chicer","Black Cat"],
+
+["Community 8","Black Cat"]
+
+],
+
+D: [
+
+["Team Cheddar","Kreo Rabels"],
+["Team Cheddar","Tilines de Guadalajara"],
+["Team Cheddar","Ultra Chmicha"],
+["Team Cheddar","TPE"],
+["Team Cheddar","Los Indestructibles"],
+
+["Kreo Rabels","Tilines de Guadalajara"],
+["Kreo Rabels","Ultra Chmicha"],
+["Kreo Rabels","TPE"],
+["Kreo Rabels","Los Indestructibles"],
+
+["Tilines de Guadalajara","Ultra Chmicha"],
+["Tilines de Guadalajara","TPE"],
+["Tilines de Guadalajara","Los Indestructibles"],
+
+["Ultra Chmicha","TPE"],
+["Ultra Chmicha","Los Indestructibles"],
+
+["TPE","Los Indestructibles"]
+
+]
+
+};
