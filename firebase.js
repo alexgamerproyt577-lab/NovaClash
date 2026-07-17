bdec182aa9dc36a626541d839f649f45bb3b6fe6
alexgamerproyt577-lab <alexgamerproyt577@gmail.com>
2026-07-17 18:48:46 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,17 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
-
-import {
-    getFirestore
-} from "https://www.gstatic.com/firebasejs/12.5.0/firebase-firestore.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
+
+const firebaseConfig = {
+
+  // tu configuración
+
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+
 export const auth = getAuth(app);
 
 const firebaseConfig = {
