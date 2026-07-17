9b08a10fd06ff0eae890ba3eab82c5646b99aeef
alexgamerproyt577-lab <alexgamerproyt577@gmail.com>
2026-07-17 19:39:06 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/12.5.0/firebase
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCULLdWfSlZL2vNetwknkaEOj5fAsIIr6o",
-    authDomain: "nova-clash-8c78b.firebaseapp.com",
-    projectId: "nova-clash-8c78b",
-    storageBucket: "nova-clash-8c78b.firebasestorage.app",
-    messagingSenderId: "202679377197",
-    appId: "1:202679377197:web:b5ed1526028cac92aa07a4"
+  apiKey: "AIzaSyCULLdWfSlZL2vNetwknkaEOj5fAsTIr6o",
+  authDomain: "nova-clash-8c78b.firebaseapp.com",
+  projectId: "nova-clash-8c78b",
+  storageBucket: "nova-clash-8c78b.firebasestorage.app",
+  messagingSenderId: "202679377197",
+  appId: "1:202679377197:web:b5ed1526028cac92aa07a4"
 };
 
 // Inicializar Firebase
